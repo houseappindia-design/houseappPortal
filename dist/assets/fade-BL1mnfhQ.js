968f883726b16efbc688060c0463eabf66b039b6
vikas kumar <vik23641@gmail.com>
2025-09-29 17:28:42 +0530

    code is updated

---
+++ dist/assets/fade-BL1mnfhQ.js
@@ -0,0 +1,4 @@
+import{a4 as e}from"./button-Dt2JUpkg.js";import"./index-DpkaZWrN.js";import{Q as r}from"./BaseInput-B3LhqOGP.js";const m=new e("antFadeIn",{"0%":{opacity:0},"100%":{opacity:1}}),s=new e("antFadeOut",{"0%":{opacity:1},"100%":{opacity:0}}),f=function(a){let i=arguments.length>1&&arguments[1]!==void 0?arguments[1]:!1;const{antCls:o}=a,n=`${o}-fade`,t=i?"&":"";return[r(n,m,s,a.motionDurationMid,i),{[`
+        ${t}${n}-enter,
+        ${t}${n}-appear
+      `]:{opacity:0,animationTimingFunction:"linear"},[`${t}${n}-leave`]:{animationTimingFunction:"linear"}}]};export{f as i};
